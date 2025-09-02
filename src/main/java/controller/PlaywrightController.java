49f7326e9793e7e36bc26aa4c3c4cde696022831
Maharudra Yadav <88700864+maharudrayadav@users.noreply.github.com>
2025-09-02 14:17:03 +0530

    Update PlaywrightController.java

--- src/main/java/controller/PlaywrightController.java
+++ src/main/java/controller/PlaywrightController.java
@@ -18,7 +18,7 @@ public class PlaywrightController {
         String username = body.get("username");
         String password = body.get("password");
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             Page page = browser.newPage();
             page.navigate("https://www.naukri.com/mnjuser/login");
 
